ae3ff89dc2c28d2006c7425d7ed42f910b9c2296
Abián Izquierdo <abizmo@gmail.com>
2022-03-13 22:11:45 +0000

    Feat: Add template of Details component

--- src/components/Details/index.jsx
+++ src/components/Details/index.jsx
@@ -1,13 +1,34 @@
+import { useState } from 'react';
+
 function Details() {
+  const [ip] = useState('192.212.174.101');
+  const [location] = useState('Brooklyn, NY 10001');
+  const [timezone] = useState('UTC');
+  const [offset] = useState('-05:00');
+  const [isp] = useState('SpaceX Starlink');
+
   return (
     <div>
-      IP Address
-      Location
-      Timezone
-      UTC
-      {' '}
-      {/* add offset value dynamically using the API */}
-      ISP
+      <div>
+        <h2>IP Address</h2>
+        <p>{ip}</p>
+      </div>
+      <div>
+        <h2>Location</h2>
+        <p>{location}</p>
+      </div>
+      <div>
+        <h2>Timezone</h2>
+        <p>
+          {timezone}
+          {' '}
+          {offset}
+        </p>
+      </div>
+      <div>
+        <h2>ISP</h2>
+        <p>{isp}</p>
+      </div>
     </div>
   );
 }
