d4367016b092784f44e4fdfccc5eccaed2ffe272
Abián Izquierdo <abizmo@gmail.com>
2022-03-16 16:45:10 +0000

    Fix: Change trackerService's parameters

--- src/services/tracker.js
+++ src/services/tracker.js
@@ -3,10 +3,14 @@ import axios from 'axios';
 const API_KEY = process.env.REACT_APP_GEO_IPIFY_API_KEY;
 const BASE_URL = 'https://geo.ipify.org/api/v2';
 
-async function track(query, value) {
+async function track({ domain, ipAddress } = {}) {
   try {
+    let query = '';
+    query = ipAddress ? `&ipAddress=${ipAddress}` : query;
+    query = domain ? `${query}&domain=${domain}` : query;
+
     const { status, statusText, data } = await axios
-      .get(`${BASE_URL}/country,city?apiKey=${API_KEY}&${query}=${value}`);
+      .get(`${BASE_URL}/country,city?apiKey=${API_KEY}${query}`);
 
     if (status !== 200) {
       throw new Error(statusText);
