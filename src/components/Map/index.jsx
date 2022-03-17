3ae9463759e02a9c577eacaf42566d5e71b55b9a
Abián Izquierdo <abizmo@gmail.com>
2022-03-17 21:34:13 +0000

    Feat: Add map

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,6 +1,39 @@
+import { icon } from 'leaflet';
+import { useEffect, useState } from 'react';
+import {
+  MapContainer, Marker, TileLayer,
+} from 'react-leaflet';
+import { useSelector } from 'react-redux';
+
+import iconLocation from '../../assets/images/icon-location.svg';
+
+const IconLocation = icon({ iconUrl: iconLocation });
+
+const { REACT_APP_MAP_TOKEN } = process.env;
+
 function Map() {
+  const [map, setMap] = useState(null);
+  const { lat, lng } = useSelector(({ tracker }) => tracker);
+  const position = [lat, lng];
+
+  useEffect(() => {
+    if (map) map.setView([lat, lng], 16);
+  }, [lat, lng]);
+
   return (
-    <div style={{ backgroundColor: 'hsl(55 82% 59%)' }}>map</div>
+    <MapContainer
+      center={map ? map.getCenter() : position}
+      zoom={16}
+      style={{ zIndex: -1 }}
+      zoomControl={false}
+      whenCreated={setMap}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url={`https://api.mapbox.com/styles/v1/abizmo/cl0v92jdw000t15ozt63uzsk3/tiles/{z}/{x}/{y}?access_token=${REACT_APP_MAP_TOKEN}`}
+      />
+      <Marker position={position} icon={IconLocation} />
+    </MapContainer>
   );
 }
 
